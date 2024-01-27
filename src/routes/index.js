6c33dafcbb23e0dedfc18e07305afa44989a7b61
phullt <phuqn10xxx@gmail.com>
2024-01-27 13:05:38 +0700

    up

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import Home from '../pages/Home'
+import Following from '../pages/Following'
+import Profile from '../pages/Profile'
+import Upload from '../pages/Upload'
+import Search from '../pages/Search'
+import {HeaderOnly} from '../components/Layout'
+const publicRoutes = [
+    {path: '/',component : Home},
+    {path: '/Following',component : Following},
+    {path: '/Profile',component : Profile},
+    {path: '/Upload',component : Upload, layout : HeaderOnly},
+    {path: '/Search',component : Search, layout : null}
+]
+
+const privateRoutes = [
+
+]
+
+export { privateRoutes , publicRoutes}
